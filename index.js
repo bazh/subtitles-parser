85c1fb45761eaf511154e09ad80224155c90c655
Adrian Ortmann <adrian.ortmann@online.de>
2014-06-28 16:18:12 +0200

    added options for get hearing impaer subtitles or not

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ var parser = (function() {
      * @param  {Boolean} ms   Optional: use milliseconds for startTime and endTime
      * @return {Array}  
      */
-    pItems.fromSrt = function(data, ms) {
-        var useMs = ms ? true : false;
+    pItems.fromSrt = function(data, options) {
+        var useMs = options.ms ? true : false;
+        var getHearingImpaerSubtitles = options.getHearingImpaerSubtitles || false;
 
         data = data.replace(/\r/g, '');
         var regex = /(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/g;
@@ -24,12 +25,26 @@ var parser = (function() {
 
         var items = [];
         for (var i = 0; i < data.length; i += 4) {
-            items.push({
-                id: data[i].trim(),
-                startTime: useMs ? timeMs(data[i + 1].trim()) : data[i + 1].trim(),
-                endTime: useMs ? timeMs(data[i + 2].trim()) : data[i + 2].trim(),
-                text: data[i + 3].trim()
-            });
+            if(getHearingImpaerSubtitles) {
+                items.push({
+                    id: data[i].trim(),
+                    startTime: useMs ? timeMs(data[i + 1].trim()) : data[i + 1].trim(),
+                    endTime: useMs ? timeMs(data[i + 2].trim()) : data[i + 2].trim(),
+                    text: data[i + 3].trim()
+                });
+            } else {
+                if(data[i + 3].trim().indexOf('[') > -1 || data[i + 3].trim().indexOf(']') > -1) {
+                    continue;
+                } else {
+                    items.push({
+                        id: data[i].trim(),
+                        startTime: useMs ? timeMs(data[i + 1].trim()) : data[i + 1].trim(),
+                        endTime: useMs ? timeMs(data[i + 2].trim()) : data[i + 2].trim(),
+                        text: data[i + 3].trim()
+                    });
+                }
+            }
+            // console.log(data[i + 3].trim());
         }
 
         return items;
